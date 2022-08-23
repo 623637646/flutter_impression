826b27e8c1ebf7b867c83753d5a9c30b2016bf99
Yanni Wang <wy19900729@gmail.com>
2022-08-23 21:00:28 +0800

    ume

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,6 +1,9 @@
 import 'package:example/home_page.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:impression/impression.dart';
+import 'package:flutter_ume/flutter_ume.dart';
+import 'package:flutter_ume_kit_perf/flutter_ume_kit_perf.dart';
 
 void main() {
   DefaultImpressionDetectorConfig.resetWhen
@@ -8,7 +11,15 @@ void main() {
   DefaultImpressionDetectorConfig.durationThreshold =
       const Duration(seconds: 1);
   DefaultImpressionDetectorConfig.visibleFractionThreshold = 0.5;
-  runApp(const MyApp());
+
+  if (kDebugMode) {
+    PluginManager.instance
+      ..register(Performance())
+      ..register(const MemoryInfoPage());
+    runApp(const UMEWidget(enable: true, child: MyApp()));
+  } else {
+    runApp(const MyApp());
+  }
 }
 
 class MyApp extends StatelessWidget {
